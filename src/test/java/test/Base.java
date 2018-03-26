7370138d591bf5c11f0431ac94b115d003a4199a
Tania <taniacascer@gmail.com>
2018-03-26 09:53:29 +0200

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,9 +31,9 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.11) {
+        if (r < 0.1) {
             fail("oops");
-       } else if (r < 0.2) {
+        } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
         }
     }
